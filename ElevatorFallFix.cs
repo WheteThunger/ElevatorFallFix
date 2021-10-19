eb4bd0ef5627b2fd0acd6e6a39101685d21f0102
WhiteThunder <whetethunger@gmail.com>
2021-10-18 22:29:12 -0700

    Add plugin

---
+++ ElevatorFallFix.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Oxide.Plugins
+{
+    [Info("Elevator Fall Fix", "WhiteThunder", "0.1.0")]
+    [Description("Fixes players falling through elevators.")]
+    internal class ElevatorFallFix : CovalencePlugin
+    {
+        private readonly FieldInfo DoClippingCheckField = typeof(TriggerParent).GetField("doClippingCheck", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private void OnServerInitialized()
+        {
+            foreach (var lift in BaseNetworkable.serverEntities.OfType<ElevatorLift>())
+            {
+                OnEntitySpawned(lift);
+            }
+        }
+
+        private void OnEntitySpawned(ElevatorLift lift)
+        {
+            if (lift is ElevatorLiftStatic)
+                return;
+
+            var parentTrigger = lift.GetComponentInChildren<TriggerParent>();
+            if (parentTrigger == null)
+                return;
+
+            DoClippingCheckField.SetValue(parentTrigger, false);
+        }
+        
+        private void Unload()
+        {
+            foreach (var lift in BaseNetworkable.serverEntities.OfType<ElevatorLift>())
+            {
+                if (lift is ElevatorLiftStatic)
+                    continue;
+
+                var parentTrigger = lift.GetComponentInChildren<TriggerParent>();
+                if (parentTrigger == null)
+                    continue;
+
+                DoClippingCheckField.SetValue(parentTrigger, true);
+            }
+        }
+    }
+}
